20b585b937f6e48583503c6ce8c6084a7e400976
shmelzz <evshelemekh@edu.hse.ru>
2022-07-05 14:59:50 +0300

    layouts for main fragment added

---
+++ app/src/main/java/com/example/debtsapp/ui/groups/GroupsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.debtsapp.ui.groups
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GroupsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is groups Fragment"
+    }
+    val text: LiveData<String> = _text
+}
